204bc4ce54dc70c8784b603de10a592cdba20087
taobao <tb03722@gmail.com>
2017-08-28 21:42:46 +0800

    add study

---
+++ study/lesson4/sample.js
@@ -0,0 +1,27 @@
+// 报数，由左到右，说开始就开始;
+
+function personA(){
+    console.log ("1");
+}
+
+function personB(){
+    console.log ("2");
+}
+
+function personC(){
+    console.log ("3");
+}
+
+function personD(){
+    console.log ("4");
+}
+
+function startBaoShu(){
+    personA();
+    personB();
+    personC();
+    personD();
+}
+
+// 为什么，这行代码直接被调用;
+startBaoShu(); 
