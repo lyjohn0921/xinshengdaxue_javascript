d3fc52edc205c5e904494a3f8d8e3d09364d8eb4
taobao <tb03722@gmail.com>
2017-08-30 13:23:39 +0800

    update practice code

--- study/lesson4/sample2.js
+++ study/lesson4/sample2.js
@@ -1,26 +1,26 @@
 //嵌套使用，第一个使用后告诉下一个;
 
-function personA(){
-    console.log ("1");
-    personB();
+function personA() {
+  console.log("1");
+  personB();
 }
 
-function personB(){
-    console.log ("2");
-    personC();
+function personB() {
+  console.log("2");
+  personC();
 }
 
-function personC(){
-    console.log ("3");
-    personD();
+function personC() {
+  console.log("3");
+  personD();
 }
 
-function personD(){
-    console.log ("4");
+function personD() {
+  console.log("4");
 }
 
-function startBaoShu(){
-    personA();
+function startBaoShu() {
+  personA();
 }
 
 startBaoShu();
--- study/lesson4/sample3.js
+++ study/lesson4/sample3.js
@@ -1,34 +1,34 @@
 // 对象调用 报数;
 
 var personA = {
-    soundOff:function (){
-        console.log ("1");
-    }
-}
+  soundOff: function() {
+    console.log("1");
+  }
+};
 
 var personB = {
-    soundOff:function (){
-        console.log ("2");
-    }
-}
+  soundOff: function() {
+    console.log("2");
+  }
+};
 
 var personC = {
-    soundOff:function (){
-        console.log ("3");
-    }
-}
+  soundOff: function() {
+    console.log("3");
+  }
+};
 
 var personD = {
-    soundOff:function (){
-        console.log ("4");
-    }
-}
+  soundOff: function() {
+    console.log("4");
+  }
+};
 
-function startBaoShu(){
-    personA.soundOff();
-    personB.soundOff();
-    personC.soundOff();
-    personD.soundOff();
+function startBaoShu() {
+  personA.soundOff();
+  personB.soundOff();
+  personC.soundOff();
+  personD.soundOff();
 }
 
 startBaoShu();
