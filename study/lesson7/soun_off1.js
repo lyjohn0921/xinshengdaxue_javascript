1ee9e12917a2253f75c0ab2d5517a86e26649421
taobao <tb03722@gmail.com>
2017-09-19 11:54:38 +0800

    update

---
+++ study/lesson7/soun_off1.js
@@ -0,0 +1,23 @@
+function allCount(){
+    var allCount = 6;
+    var number = 0;
+
+    var result =  A(number,allCount);
+    if (result == true){
+        console.log("true");
+    } 
+}
+
+function A(number,allCount){
+    console.log(number);
+    return B(number+1,allCount);
+    console.log(number);
+}
+
+function B(number,allCount){
+    console.log(number);
+    return true;
+    console.log(number);
+}
+
+allCount();
